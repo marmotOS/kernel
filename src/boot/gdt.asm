14e622a7ff3ac578f0d24c6363bc2fb1d6db269a
Bryant Moscon <bmoscon@gmail.com>
2016-02-01 19:41:21 -0500

    Update gdt.asm

--- src/boot/gdt.asm
+++ src/boot/gdt.asm
@@ -1,4 +1,4 @@
-; Copyright (C) 2013-2016  Bryant Moscon - bmoscon@gmail.com
+; Copyright (C) 2016  Bryant Moscon - bmoscon@gmail.com
 ; 
 ; Please see the LICENSE file for the terms and conditions associated with
 ; the use of this software
